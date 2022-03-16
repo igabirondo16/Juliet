818b080b3dcdd910fe415a6f9a14bde96e4021ac
Iñigo Gabirondo López <inigo.ikasle@gmail.com>
2022-03-16 16:14:11 +0100

    Implement article searches against all the documents

--- utils/ArticlesKeeper.py
+++ utils/ArticlesKeeper.py
@@ -7,7 +7,7 @@ class ArticlesKeeper(object):
         self.article = None
     
 
-    def get_news_by_category(self, category):
+    def get_articles_by_category(self, category):
         qs = QuerySearcher()
         news = qs.get_news_by_category(category)
         output_msg = "Kategoria: " + category + "\n\n"
@@ -20,12 +20,16 @@ class ArticlesKeeper(object):
 
         return output_msg
 
-    def get_new_content(self, category, user_query):
+    def get_article_content_from_category(self, category, user_query):
         qs = QuerySearcher()
         result = qs.search_article(category, user_query)
 
         if result == None:
-            return "Ez da emaitzarik aurkitu"
+            return None
 
         return result['content']
+
+    def get_article_content_from_all_news(self, user_query):
+        result = self.get_article_content_from_category('all_articles', user_query)
+        return result
 
